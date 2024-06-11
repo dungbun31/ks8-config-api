3669f17c598d6d034605905d5ee694e3fda5bbd4
dungx_buns <78593795+dungbun31@users.noreply.github.com>
2024-06-12 00:52:35 +0300

    Create config.py
    read and write NEW_TAG

---
+++ config.py
@@ -0,0 +1,29 @@
+import os
+import yaml
+
+def update_image_tag(file_path, new_tag):
+    # Đọc nội dung file YAML
+    with open(file_path, 'r') as file:
+        data = yaml.safe_load(file)
+
+    # Cập nhật giá trị của image.tag
+    data['image']['tag'] = new_tag
+
+    # Ghi lại file YAML với giá trị đã cập nhật
+    with open(file_path, 'w') as file:
+        yaml.safe_dump(data, file)
+
+if __name__ == "__main__":
+    # Đường dẫn đến file values.yaml
+    file_path = 'values.yaml'
+
+    # Đọc giá trị tag mới từ biến môi trường NEW_TAG
+    new_tag = os.getenv('NEW_TAG')
+
+    if new_tag is None:
+        print("Environment variable NEW_TAG is not set")
+        exit(1)
+
+    # Gọi hàm để cập nhật giá trị tag
+    update_image_tag(file_path, new_tag)
+
