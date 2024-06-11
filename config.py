5fd22ae98bf71a8e044e8139fb02bf2eb08fd13a
dungx_buns <78593795+dungbun31@users.noreply.github.com>
2024-06-12 01:58:13 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,29 @@
 import os
-import yaml
+from ruamel.yaml import YAML
+
 
 def update_image_tag(file_path, new_tag):
+    yaml = YAML()
+    yaml.preserve_quotes = True
+
     # Đọc nội dung file YAML
-    with open(file_path, 'r') as file:
-        data = yaml.safe_load(file)
+    with open(file_path, "r") as file:
+        data = yaml.load(file)
 
     # Cập nhật giá trị của image.tag
-    data['image']['tag'] = new_tag
+    data["image"]["tag"] = new_tag
 
     # Ghi lại file YAML với giá trị đã cập nhật
-    with open(file_path, 'w') as file:
-        yaml.safe_dump(data, file)
+    with open(file_path, "w") as file:
+        yaml.dump(data, file)
+
 
 if __name__ == "__main__":
     # Đường dẫn đến file values.yaml
-    file_path = 'values.yaml'
+    file_path = "values.yaml"
 
     # Đọc giá trị tag mới từ biến môi trường NEW_TAG
-    new_tag = os.getenv('NEW_TAG')
+    new_tag = os.getenv("NEW_TAG")
 
     if new_tag is None:
         print("Environment variable NEW_TAG is not set")
@@ -26,4 +31,3 @@ if __name__ == "__main__":
 
     # Gọi hàm để cập nhật giá trị tag
     update_image_tag(file_path, new_tag)
-
